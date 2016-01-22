4d530bc556df79f79a0cae8a3a75d0136d9918e9
georoot <me@rahulbhola.in>
2016-01-22 12:11:59 +0000

    addition of append function and rename of typo inerHTML to innerHTML

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var typejs = function(idx){
 	//FIXME: this can be a class also
 	this.console = document.getElementById(idx);
-	this.currentText = this.console.inerHTML;
+	this.currentText = this.console.innerHTML;
 	//Default speed in case none is given
 	this.operationSpeed = 50;
 };
@@ -12,5 +12,15 @@ typejs.prototype.setSpeed = function(speedx) {
 	this.operationSpeed = speedx;
 };
 
+typejs.prototype.append = function(data) {
+	this.console.innerHTML = this.console.innerHTML + data.charAt(0);
+	var currentInstance = this;
+	if (data.length > 1){
+		var remString = data.substr(1,data.length);
+		setTimeout(function(){currentInstance.append(remString);},currentInstance.operationSpeed);
+	}
+};
+
 var w = new typejs("consoleContent");
 w.setSpeed(200);
+w.append("hello typejs!");
